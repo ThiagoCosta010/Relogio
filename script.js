acf634ea65b496ee67510c655e407f83294fa6de
Thiago Costa <tigurrock@gmail.com>
2022-11-16 11:43:39 -0300

    att

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const handHours = document.querySelector('.hand.hours')
 const setRotation = (element, rotationPercentage) => {
     element.style.setProperty('--rotation', rotationPercentage * 360)
 }
+
 function setClock(){
     const currentData = new Date()
 
     const secondsPercentage = currentData.getSeconds() / 60
     const minutesPercentage = (secondsPercentage + currentData.getMinutes()) / 60
-    const hoursPercentage = (minutesPercentage + currentData.getHours()) / 12
+    const hoursPercentage = (minutesPercentage + currentData.getDate()) / 12
 
-    setRotation(handSeconds, secondsPercentage)
+    setRotation(handSeconds, secondsPercentage )
     setRotation(handMinutes, minutesPercentage)
     setRotation(handHours, hoursPercentage)
 }
